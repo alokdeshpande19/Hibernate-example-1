a851c78e77135d9b8fbb2c0f8c9bc00485e9b501
madhukar <madhukarpandey007@gmail.com>
2013-09-13 15:19:53 +0530

    PostgreSql Connection using Hibernate

---
+++ src/com/javatpoint/mypackage/StoreData.java
@@ -0,0 +1,29 @@
+package com.javatpoint.mypackage;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.cfg.Configuration;
+
+public class StoreData {
+	public static void main(String[] args) {
+		Configuration cfg = new AnnotationConfiguration();
+		cfg.configure("hibernate.cfg.xml");
+		SessionFactory factory = cfg.buildSessionFactory();
+		Session session = factory.openSession();
+		Transaction t = session.beginTransaction();
+		UserDbo user = new UserDbo();
+		user.setEmail("ma@gmail.com");
+		user.setPassword("hh");
+		user.setAdmin(true);
+		session.persist(user);
+		session.flush();
+		System.out.println("hhh");
+		t.commit();
+		session.close();
+
+		System.out.println("successfully saved");
+
+	}
+}
