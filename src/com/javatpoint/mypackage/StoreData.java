8df29123dfdfe80f23f40944102f35ba6cff830d
madhukar <madhukarpandey007@gmail.com>
2013-11-27 17:13:48 +0530

    using hsqldb

--- src/com/javatpoint/mypackage/StoreData.java
+++ src/com/javatpoint/mypackage/StoreData.java
@@ -7,8 +7,7 @@ import org.hibernate.cfg.AnnotationConfiguration;
 import org.hibernate.cfg.Configuration;
 
 public class StoreData {
-	@SuppressWarnings("deprecation")
-	public static void main(String[] args) {
+	   public static void main(String[] args) {
 		Configuration cfg = new AnnotationConfiguration();
 		cfg.configure("hibernate.cfg.xml");
 		SessionFactory factory = cfg.buildSessionFactory();
@@ -17,13 +16,10 @@ public class StoreData {
 		UserDbo user = new UserDbo();
 		user.setEmail("ma@gmail.com");
 		user.setPassword("hh");
-		user.setAdmin(true);
 		session.persist(user);
 		session.flush();
-		System.out.println("hhh");
 		t.commit();
 		session.close();
-
 		System.out.println("successfully saved");
 
 	}
--- src/com/javatpoint/mypackage/UserDbo.java
+++ src/com/javatpoint/mypackage/UserDbo.java
@@ -10,8 +10,6 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
-
-
 @Entity
 public class UserDbo {
 
@@ -23,38 +21,18 @@ public class UserDbo {
 
 	private String password;
 
-	private String firstName;
-
-	private String lastName;
-
-	private String phone;
-	
-	private boolean isAdmin;
-
-	
-	@ManyToOne
-	private CompanyDbo company;
-    
-	@OneToOne
-	private UserDbo manager;
-	
-	@OneToMany
-	private Set<UserDbo> employees = new HashSet<UserDbo>();
-
-
-	public Integer getId() {
+	public int getId() {
 		return id;
 	}
 
-	public void setId(Integer id) {
+	public void setId(int id) {
 		this.id = id;
 	}
 
 	public String getEmail() {
 		return email;
 	}
-	
-	
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
@@ -67,79 +45,5 @@ public class UserDbo {
 		this.password = password;
 	}
 
-	public CompanyDbo getCompany() {
-		return company;
-	}
-
-	public void setCompany(CompanyDbo company) {
-		this.company = company;
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	public String getPhone() {
-		return phone;
-	}
-
-	public void setPhone(String phone) {
-		this.phone = phone;
-	}
-
-	public UserDbo getManager() {
-		return manager;
-	}
-
-	public void setManager1(UserDbo manager) {
-		this.manager = manager;
-	}
-
-	public Set<UserDbo> getEmployees() {
-		return employees;
-	}
-
-	public void setEmployees(Set<UserDbo> employees) {
-		this.employees = employees;
-	}
-
-/*	public void addEmployee(UserDbo employee) {
-		this.employees.add(employee);
-	}
-
-	public void deleteEmployee(UserDbo employee) {
-		this.employees.remove(employee);
-	}*/
-
-	/*public Set<TimeCardDbo> getTimecards() {
-		return timecards;
-	}
-
-	public void setTimecards(Set<TimeCardDbo> timecards) {
-		this.timecards = timecards;
-	}
-
-	public void addTimecards(TimeCardDbo timecard) {
-		this.timecards.add(timecard);
-	}*/
-
-	public boolean isAdmin() {
-		return isAdmin;
-	}
-
-	public void setAdmin(boolean isAdmin) {
-		this.isAdmin = isAdmin;
-	}
+	
 }
