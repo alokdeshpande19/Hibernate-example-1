f93f327bfd922272e33d113ea96d565fb7c0c9df
madhukar <madhukarpandey007@gmail.com>
2013-11-27 12:28:48 +0530

    changes for hibernate4.1.3

--- src/com/javatpoint/mypackage/StoreData.java
+++ src/com/javatpoint/mypackage/StoreData.java
@@ -7,6 +7,7 @@ import org.hibernate.cfg.AnnotationConfiguration;
 import org.hibernate.cfg.Configuration;
 
 public class StoreData {
+	@SuppressWarnings("deprecation")
 	public static void main(String[] args) {
 		Configuration cfg = new AnnotationConfiguration();
 		cfg.configure("hibernate.cfg.xml");
