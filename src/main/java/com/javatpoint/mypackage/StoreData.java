391627f6c2bd4fb6a9fe3a96f57b9d454614840e
Dean Hiller <dean@alvazan.com>
2013-11-30 11:12:12 -0700

    add log4jdbc and logback logging as well

--- src/main/java/com/javatpoint/mypackage/StoreData.java
+++ src/main/java/com/javatpoint/mypackage/StoreData.java
@@ -1,48 +1,62 @@
 package com.javatpoint.mypackage;
 
-import java.util.Iterator;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-import org.hibernate.annotations.FlushModeType;
-
 public class StoreData {
-	   static EntityManagerFactory emf = Persistence.createEntityManagerFactory("hsqldb-ds");
-	   
-	   public static void main(String[] args) {
-		  //for inserting the data
-		   EntityManager entityManager = emf.createEntityManager();
-		   entityManager.getTransaction().begin();
-		   //entityManager.setFlushMode(FlushModeType.MANUAL);
-		   
-		   UserDbo user=new UserDbo();
-		   user.setEmail("madhukar@easility.com");
-		   user.setPassword("easility");
-		   entityManager.persist(user);
-		   UserDbo user1=new UserDbo();
-		   user1.setEmail("madhukarpandey007@gmail.com");
-		   user1.setPassword("easility123");
-		   entityManager.persist(user1);
-		   
-		   entityManager.flush();
-		   entityManager.flush();
-		   
-		   entityManager.getTransaction().commit();
-		   entityManager.close();
-	     
-		   // for fetching the data 	   
-		   entityManager = emf.createEntityManager();
-		   entityManager.getTransaction().begin();
-		   List<UserDbo> result = entityManager.createQuery( "from UserDbo", UserDbo.class ).getResultList();
-		   for ( UserDbo userDbo : result ) {
-		       System.out.println( "UserId=" + userDbo.getId());
-		       System.out.println( "User EmailId=" + userDbo.getEmail());
-		       System.out.println( "User Password=" + userDbo.getPassword());
-		   }
-		   entityManager.getTransaction().commit();
-		   entityManager.close();
-	   }
+	static EntityManagerFactory emf;
+
+	public static void main(String[] args) {
+		try {
+			Class.forName("net.sf.log4jdbc.DriverSpy");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+
+		emf = Persistence.createEntityManagerFactory("hsqldb-ds");
+		// for inserting the data
+		EntityManager entityManager = emf.createEntityManager();
+		entityManager.getTransaction().begin();
+		// entityManager.setFlushMode(FlushModeType.MANUAL);
+
+		UserDbo user = new UserDbo();
+		user.setEmail("madhukar@easility.com");
+		user.setPassword("easility");
+
+		CompanyDbo companyDbo = new CompanyDbo();
+
+		user.setCompany(companyDbo);
+		companyDbo.addUser(user);
+
+		entityManager.persist(companyDbo);
+		entityManager.persist(user);
+		entityManager.flush();
+
+		UserDbo user1 = new UserDbo();
+		user1.setEmail("madhukarpandey007@gmail.com");
+		user1.setPassword("easility123");
+		entityManager.persist(user1);
+
+		entityManager.flush();
+
+		entityManager.getTransaction().commit();
+		entityManager.close();
+
+		// for fetching the data
+		entityManager = emf.createEntityManager();
+		entityManager.getTransaction().begin();
+		List<UserDbo> result = entityManager.createQuery("from UserDbo",
+				UserDbo.class).getResultList();
+		for (UserDbo userDbo : result) {
+			System.out.println("UserId=" + userDbo.getId());
+			System.out.println("User EmailId=" + userDbo.getEmail());
+			System.out.println("User Password=" + userDbo.getPassword());
+		}
+		entityManager.getTransaction().commit();
+		entityManager.close();
+	}
 }
--- src/main/java/com/javatpoint/mypackage/UserDbo.java
+++ src/main/java/com/javatpoint/mypackage/UserDbo.java
@@ -1,29 +1,97 @@
 package com.javatpoint.mypackage;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EntityManager;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.NoResultException;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.PrePersist;
+import javax.persistence.Query;
+
+import org.hibernate.annotations.Index;
 
 @Entity
+@NamedQueries({
+	@NamedQuery(name = "findAllField", query = "select u from UserDbo as u"),
+	@NamedQuery(name = "findByEmailId", query = "select u from UserDbo as u where u.email=:email") })
 public class UserDbo {
 
-    @Id
-    @GeneratedValue
-    private int id;
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Integer id;
 
+	@Index(name="entityIndexCol")
+	@Column(unique = true)
 	private String email;
 
+	private String phone;
 	private String password;
+	private String name;
+	private String firstName;
+	private String lastName;
 
-	public int getId() {
+	@ManyToOne
+	@JoinColumn(nullable=false)
+	private CompanyDbo company;
+
+	@ManyToOne
+	private UserDbo manager;
+	
+	@OneToMany(mappedBy = "manager")
+	private List<UserDbo> employees = new ArrayList<UserDbo>();
+
+	private boolean isNewPasswordChange;
+
+	public boolean isNewPasswordChange() {
+		return isNewPasswordChange;
+	}
+
+	public void setNewPasswordChange(boolean isNewPasswordChange) {
+		this.isNewPasswordChange = isNewPasswordChange;
+	}
+
+	public Integer getId() {
 		return id;
 	}
 
-	public void setId(int id) {
+	public List<UserDbo> getEmployees() {
+		return employees;
+	}
+
+	public void setEmployees(List<UserDbo> employees) {
+		this.employees = employees;
+	}
+
+	public void addEmployee(UserDbo employee) {
+		this.employees.add(employee);
+	}
+
+	public void deleteEmployee(UserDbo employee) {
+		this.employees.remove(employee);
+	}
+
+	public void setId(Integer id) {
 		this.id = id;
+	}
+
+	public CompanyDbo getCompany() {
+		return company;
+	}
+
+	public void setCompany(CompanyDbo company) {
+		this.company = company;
 	}
 
 	public String getEmail() {
@@ -42,5 +110,57 @@ public class UserDbo {
 		this.password = password;
 	}
 
-	
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public UserDbo getManager() {
+		return manager;
+	}
+
+	public void setManager(UserDbo manager) {
+		this.manager = manager;
+	}
+
+	public static List<UserDbo> findAllField(EntityManager mgr) {
+		Query query = mgr.createNamedQuery("findAll");
+		return query.getResultList();
+	}
+
+	public static UserDbo findByEmailId(EntityManager mgr, String email) {
+		Query query = mgr.createNamedQuery("findByEmailId");
+		query.setParameter("email", email);
+		try {
+			return (UserDbo) query.getSingleResult();
+		} catch (NoResultException e) {
+			return null;
+		}
+	}
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
 }
